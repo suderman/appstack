6d4ff530d658b9eb0a30e3c875d79ce8bdbbb807
Jon Suderman <suderman@gmail.com>
2013-01-07 14:12:05 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-# 2012 Jon Suderman
+# 2013 Jon Suderman
 # https://github.com/suderman/appstack/
 
 # Open a terminal and run this command:
@@ -9,13 +9,16 @@
 # Ensure /usr/local/bin exists
 if [ ! -d "/usr/local" ]; then
   sudo mkdir -p /usr/local/bin
-  sudo chown `whoami`:admin /usr/{local,local/bin}
+  ULB_OWNER=`whoami`:`ls -ld /usr | awk '{print $4}'`
+  sudo chown $ULB_OWNER /usr/{local,local/bin}
 fi
 
 # Download appstack into /usr/local/bin
-curl https://raw.github.com/suderman/appstack/master/appstack -o /usr/local/bin/appstack
+sudo curl https://raw.github.com/suderman/appstack/master/appstack -o /usr/local/bin/appstack
 
 # Set permissions
-chmod +x /usr/local/bin/appstack
+ULB_OWNER=`ls -ld /usr/local/bin | awk '{print $3}'`:`ls -ld /usr/local/bin | awk '{print $4}'`
+sudo chown $ULB_OWNER /usr/local/bin/appstack 
+sudo chmod +x /usr/local/bin/appstack
 
 echo "Installed appstack to /usr/local/bin"
